cdc9fd4d2678f969dfa512533881cc91ca995bff
elyis <elyi7367@gmail.com>
2024-07-16 04:00:43 -0700

    add init data

--- src/STELA_CONTENT.Api/Program.cs
+++ src/STELA_CONTENT.Api/Program.cs
@@ -1,11 +1,11 @@
 using System.Text;
-using dotenv.net;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using STELA_CONTENT.App.Service;
+using STELA_CONTENT.Core.Entities.Models;
 using STELA_CONTENT.Core.IService;
 using STELA_CONTENT.Infrastructure.Data;
 using STELA_CONTENT.Infrastructure.Service;
@@ -18,6 +18,7 @@ var app = builder.Build();
 ConfigureMiddleware(app);
 
 ApplyMigrations(app);
+InitDatabase(app);
 
 app.MapGet("/", () => "Content server is working!");
 
@@ -159,4 +160,142 @@ void ApplyMigrations(WebApplication app)
         var logger = services.GetRequiredService<ILogger<Program>>();
         logger.LogError(ex, "An error occurred while migrating the database.");
     }
+}
+
+void InitDatabase(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ContentDbContext>();
+
+    var countAdditionalServices = context.AdditionalServices.Count();
+    if (countAdditionalServices != 0)
+        return;
+
+    context.AdditionalServices.AddRangeAsync(new List<AdditionalService>
+    {
+        new() { Name = "Мемориальные комплексы", Price = 10000 },
+        new() { Name = "Изготовление скульптур", Price = 10000 },
+        new() { Name = "Эксклюзивные памятники", Price = 10000 }
+    });
+
+    var materials = new List<MemorialMaterial>()
+    {
+        new() { Name = "Шанси Блэк", ColorName = "Черный"},
+        new() { Name = "Змеевик", ColorName = "Зеленый"},
+        new() { Name = "Габбро-Диабаз", ColorName = "Черный", Hex = "484848"},
+        new() { Name = "Диабаз", ColorName = "Серый"},
+        new() { Name = "Коелга", ColorName = "Белый"},
+        new() { Name = "Абсолют Блэк", ColorName = "Черный"},
+        new() { Name = "Олив Грин", ColorName = "Зеленый", Hex = "549079" },
+        new() { Name = "Паданг Дарк", ColorName = "Серый" },
+        new() { Name = "Роял Уайт", ColorName = "Белый"},
+        new() { Name = "Чайна Марбал Уайт", ColorName = "Белый"},
+        new() { Name = "Империал Рэд", ColorName = "Красный"},
+        new() { Name = "Куру Грей", ColorName = "Серый"}
+    };
+    context.Materials.AddRange(materials);
+
+    context.Memorials.AddRange(new List<Memorial>()
+    {
+        new()
+        {
+            Name = "Проект 1",
+            Price = 40000,
+            Description = "",
+            StelaHeight = 1,
+            StelaLength = 1,
+            StelaWidth = 1,
+            Materials = new List<MemorialMaterials>()
+            {
+                new() { Material = materials[0]}
+            }
+        },
+
+        new()
+        {
+            Name = "Проект 2",
+            Price = 40000,
+            Description = "",
+            StelaHeight = 1,
+            StelaLength = 1,
+            StelaWidth = 1,
+            Materials = new List<MemorialMaterials>()
+            {
+                new() { Material = materials[3]},
+            }
+        },
+
+        new()
+        {
+            Name = "Проект 3",
+            Price = 40000,
+            Description = "",
+            StelaHeight = 1,
+            StelaLength = 1,
+            StelaWidth = 1,
+            Materials = new List<MemorialMaterials>()
+            {
+                new() { Material = materials[5]},
+                new() { Material = materials[9]},
+            }
+        },
+
+        new()
+        {
+            Name = "Проект 4",
+            Price = 40000,
+            Description = "",
+            StelaHeight = 1,
+            StelaLength = 1,
+            StelaWidth = 1,
+            Materials = new List<MemorialMaterials>()
+            {
+                new() { Material = materials[10]},
+                new() { Material = materials[11]},
+            }
+        }
+    });
+
+    var portfolioMemorials = new List<PortfolioMemorial>()
+    {
+        new() { CemeteryName = "Православное кладбище", Name = "МОНУМЕНТАЛЬНЫЙ МЕМОРИАЛЬНЫЙ КОМПЛЕКС", Description = "", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[0]}
+        }},
+
+        new() { CemeteryName = "Православное кладбище",Description = "", Name = "ЭКСКЛЮЗИВНЫЙ ПАМЯТНИК ИЗ ЗМЕЕВИКА", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[1]},
+            new() { Material = materials[2]}
+        }},
+
+        new() { CemeteryName = "Православное кладбище",Description = "", Name = "МЕМОРИАЛЬНЫЙ КОМПЛЕКС", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[1]},
+            new() { Material = materials[3]},
+            new() { Material = materials[4]}
+        }},
+
+        new() { CemeteryName = "Православное кладбище",Description = "", Name = "МЕМОРИАЛЬНЫЙ КОМПЛЕКС", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[5]},
+            new() { Material = materials[6]},
+        }},
+
+        new() { CemeteryName = "Православное кладбище",Description = "", Name = "АВТОРСКАЯ НАДГРОБНАЯ КОМПОЗИЦИЯ", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[7]},
+        }},
+
+        new() { CemeteryName = "Православное кладбище",Description = "", Name = "ЭЛИТНЫЙ МЕМОРИАЛЬНЫЙ КОМПЛЕКС", Materials = new List<PortfolioMemorialMaterials>()
+        {
+            new() { Material = materials[0]},
+            new() { Material = materials[8]},
+        }}
+    };
+
+    context.PortfolioMemorials.AddRange(portfolioMemorials);
+    context.SaveChanges();
 }
